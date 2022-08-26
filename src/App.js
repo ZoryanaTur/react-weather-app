75cbc5cd3675c4a5efdbe82ef7982db370ec4fe0
Zoryana Tur <106607876+ZoryanaTur@users.noreply.github.com>
2022-08-26 19:50:39 +0300

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,22 +3,24 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://idyllic-unicorn-26008a.netlify.app/" target="_blank">
-          {" "}
-          Zoryana Tur
-        </a>{" "}
-        {""}
-        and {""}{" "}
-        <a
-          href="https://github.com/ZoryanaTur/react-weather-app"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://idyllic-unicorn-26008a.netlify.app/" target="_blank">
+            {" "}
+            Zoryana Tur
+          </a>{" "}
+          {""}
+          and {""}{" "}
+          <a
+            href="https://github.com/ZoryanaTur/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
